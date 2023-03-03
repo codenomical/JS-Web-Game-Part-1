199f30f86a9f98d2e0e4b50cab9cb42c317e73f7
Richard Quizon <codenomics@proton.me>
2023-03-02 17:59:07 -0800

    polished for final submission

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+// Some issues. Had to rename the shield.png because it was spelled sheild.png.
+
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+// Was having trouble trying to add the code for lines 1 to 8 and 28 to 33. Had to delete my original code and add the background code with the original on notepad. Finally was able to get it to show. Got the rest without the solution, but by collaborating with other students.
+
+function newImage(url, left, bottom){
+    let pic = document.createElement('img')
+    pic.src = url
+    pic.style.position = 'fixed'
+    pic.style.left = left + 'px'
+    pic.style.bottom = bottom + 'px'
+    document.body.append(pic)
+    return pic
+}
+
+function newItem(url, left, bottom){
+    let pic = newImage(url, left, bottom)
+
+    pic.addEventListener('dblclick', () => {
+        pic.remove()
+    })
+}
+
+// Bonus Challenge
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon
+
+tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
+tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+// Added a sky island.
+newImage('assets/boulder.png', 550, 700)
+newImage('assets/boulder.png', 400, 700)
+newImage('assets/boulder.png', 250, 700)
+newImage('assets/pillar.png', 555, 700)
+newImage('assets/pillar.png', 260, 700)
+newImage('assets/tree.png', 425, 775)
+newImage('assets/green-character/east.gif', 400, 740)
+newImage('assets/crate.png', 470, 750)
+// Added a sky island.
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+// Note to self: Go back and figure out more.
